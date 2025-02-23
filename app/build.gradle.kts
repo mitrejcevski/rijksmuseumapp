6f115ecfd01b9f5bf9ba639f85fae2c9f38c533f
Jovche Mitrejchevski <mitrejcevski@gmail.com>
2025-02-23 12:28:28 +0100

    Project Definition

---
+++ app/build.gradle.kts
@@ -0,0 +1,63 @@
+plugins {
+  alias(libs.plugins.android.application)
+  alias(libs.plugins.kotlin.android)
+  alias(libs.plugins.kotlin.compose)
+}
+
+android {
+  namespace = "nl.jovmit.rmapp"
+  compileSdk = libs.versions.targetSdkVersion.get().toInt()
+
+  defaultConfig {
+    applicationId = "nl.jovmit.rmapp"
+    minSdk = libs.versions.minSdkVersion.get().toInt()
+    targetSdk = libs.versions.targetSdkVersion.get().toInt()
+    versionCode = 1
+    versionName = "1.0"
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+
+  buildFeatures {
+    compose = true
+  }
+
+  compileOptions {
+    val javaVersion = libs.versions.javaVersion.get()
+    sourceCompatibility = JavaVersion.toVersion(javaVersion)
+    targetCompatibility = JavaVersion.toVersion(javaVersion)
+  }
+
+  kotlinOptions {
+    jvmTarget = libs.versions.javaVersion.get()
+  }
+
+  testOptions.unitTests {
+    isReturnDefaultValues = true
+    all { tests ->
+      tests.useJUnitPlatform()
+    }
+  }
+}
+
+dependencies {
+  implementation(platform(libs.androidx.compose.bom))
+  implementation(libs.bundles.androidx)
+  implementation(libs.bundles.compose)
+
+  debugImplementation(libs.bundles.compose.debug)
+
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.bundles.ui.test)
+
+  testImplementation(libs.bundles.unit.test)
+
+  testRuntimeOnly(libs.junit.jupiter.engine)
+}
---
+++ app/src/test/java/nl/jovmit/rmapp/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package nl.jovmit.rmapp
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+
+class ExampleUnitTest {
+
+  @Test
+  fun additionIsCorrect() {
+    val result = 2 + 2
+    assertThat(result).isEqualTo(4)
+  }
+}
